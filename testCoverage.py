b30e825098ba25f282be0759221ea9026c3ce56e
Kiran Eiden <kiran.eiden@stonybrook.edu>
2018-06-26 12:21:48 -0400

    Updated testCoverage.py with the commits from Pat Payne's fork

--- testCoverage.py
+++ testCoverage.py
@@ -3,37 +3,77 @@ Start Date: 02/21/18
 
 Author: Patrick Payne
 
-Purpose: Check all of the tests for MAESTRO to see if every parameter
-         has been tested at a value different from the default value
+Purpose: Check all of the tests for MAESTRO or CASTRO to see if every
+         parameter has been tested at a value different from the 
+         default value
 
-MAESTRO returns a maestro-overview.out file that lists all of the runtime 
- parameters that were used in the simulation. The values that were set 
- different from the default values are marked with a [*]. This is the
- feature that we are looking for to determine if the quantity is covered. 
+In order for this script to function: the output of the tests that it is 
+ looking at should be in a form similar to what is shown below:
 
-First read the list of all of the possible parameters and then compare and
- strike the list items as we progress through the parameters that were tested.
- Finally, output a coverage file that lists the parameters that were covered
- and the parameters that were not covered in the test suite.
+[START EXAMPLE]
 
+===============================================================================
+ Runtime Parameter Information
+===============================================================================
+[*] indicates overridden default
+[*]             parameter_alpha = 1.000
+                  parameter_one = F
+
+[END EXAMPLE]
+
+The '[*]' can be preceeded by a space, as is done in MAESTRO (04/24/18), but
+ this is not necessary. The script searches for the keyword "Parameter" in
+ to begin reading the parameters immediately following the line "[*] indicates
+ overridden default" and so will expect the barrier line and that line as well.
+
+As for the form of the tests, the produce plot files should be compressed in a 
+ tarfile (".tgz"). This tarfile contains a job_info file where all of the
+ runtime parameters that were used in the simulation are listed, in the form 
+ that was expressed above. The values that were set different from the default
+ values are marked with a [*]. This is the feature that we are looking for to 
+ determine if the quantity is covered. 
+
+The basic function of the script is as follows:
+ 1) All of the test directories are recorded
+ 2) These directories are checked for a file with the extension ".tgz"
+ 3) If the file is present, the job_info file is extracted
+ 4) The parameters are then read from each job_info file
+ 5) The parameters that occured with a [*] are recorded as covered and the
+    others are recorded as not_covered. All duplicates are removed
+ 6) The problem specific parameters (only used in one simulation) are recorded
+ 7) The coverage fractions are calculated
+ 8) The coverage reports are constructed with the list of parameters that were
+    not covered, the fraction of total parameters that were covered, and the
+    total number of parameters recorded.
+
+Output: Two ".out" files, one that includes any problem specific parameters
+ (parameters that are only appear in one test) and one that excludes these
+ parameters. The are titled "Coverage.out" and "Coverage-NoSpecific.out",
+ respectively.
  
-MAESTRO: There should be a 275 total parameters, this include the problem 
+MAESTRO: There should be ~260 total parameters, this includes the problem 
          specific parameters.
+
+CASTRO: There should be ~140 total parameters, this includes the problem
+        specific parameters.
 
 Placement: Place the script in the directory where the tests are and execute
            it in there as your CWD
+
+DEVELOPMENT: Add feature for user to specify the test directory on command line
+
 '''
 
 
 import os
 import re as re
 import sys
+import tarfile
 
-
-def main(file_name = 'maestro-overview.out'):
+def main():
 
     # Gets the paths of the files of interest
-    file_paths = get_files(file_name)  
+    file_paths = get_files()  
 
     # Initialize lists
     covered = []           
@@ -58,7 +98,8 @@ def main(file_name = 'maestro-overview.out'):
         covered, no_cover, ignore, ignore_master, all_params = build_master(
             covered_temp, no_cover_temp, covered,
             no_cover, ignore, ignore_master, all_params)
-        
+
+    All = remove_duplicates(All)
     # build_master needs to be executed one more time than the number of cycles
     # in the loop to complete the ignore list
     covered, no_cover, ignore, ignore_master, all_params = build_master(
@@ -78,6 +119,7 @@ def main(file_name = 'maestro-overview.out'):
                     no_cover_no_specific, covered_no_specificFrac,
                     no_cover_no_specificFrac, specific = ignore)
 
+    clean(file_paths)
     
 def get_start_line(data_file):
     # This routine finds the line number where the list of runtime
@@ -92,7 +134,7 @@ def get_start_line(data_file):
     # Finds the Runtime Section and claculates the start_line
     with open(data_file, mode='r') as data_file:
         for line in data_file:
-            if "Runtime Parameter Information" in line:
+            if "Parameter" in line:
                 # Start line of parameter lists when the first line of
                 # the file is line 1
                 start_line = counter + 3
@@ -102,6 +144,7 @@ def get_start_line(data_file):
 
          # Checks to see if the start_line has been found, else it will stop
         if start_line <= 0:
+            print(data_file)
             sys.exit("Start_Line was not identified")
         else:
             data_file.close()
@@ -125,9 +168,9 @@ def list_parameters(data_file, start_line, All):
             else:
                if r'[*]' in line:
                    # Singles out the covered parameters and writes their names
-                   parameter = re.split(r' +', line)
+                   parameter = re.split(r' +', line.replace("[*]"," "))
                    if parameter:
-                       temp = parameter[2]
+                       temp = parameter[1]
                        covered.append(temp)
                else:
                    # Takes the rest of the parameters (not covered) and writes their names
@@ -236,7 +279,7 @@ def remove_specific_params(covered, no_cover, ignore):
     return covered_temp, no_cover_temp
 
 
-def get_files(file_name):
+def get_files():
     # Returns the absolute paths of all maestro-overview.out files.
     # The argunemnt is the name of the file that we are interested
     # in reading.
@@ -247,29 +290,33 @@ def get_files(file_name):
     data = os.getcwd()
     
     # Determines tests in the most recent test
-    dirs = os.listdir(data)   
+    dirs = []
+    dirs = [ d for d in os.listdir(data) if os.path.isdir(d) ]
 
     abs_dirs = []
     file_paths = []
 
-    file_name = 'maestro-overview.out'   
-    
     for i in range(0, len(dirs)):
         # Gets absolute path to the directories
         abs_dirs.append(os.path.join(data, dirs[i]))
-        
-    for i in range(0, len(abs_dirs)):
-        # Checks if file of interest is in a directory
-        file_here = os.path.join(abs_dirs[i], file_name)
-        
-        if os.path.isfile(file_here):
-            # If it is in the directory then that path is added
-            # to the list of files
-            file_paths.append(os.path.join(abs_dirs[i], file_name))
-        else:
-            pass
 
-    # List of directories with file_name (e.g. maestro-overview.out)
+        # Gets the job_info files from .tgz files
+    for i in range(0, len(abs_dirs)):
+        for file in os.listdir(abs_dirs[i]):
+            # Finds the tar files and extracts only job_info file
+            if file.endswith(".tgz"):
+                tmp = os.path.join(abs_dirs[i], file)
+                file_name = os.path.join(data,
+                                         os.path.splitext(file)[0])
+                # Extracts the job_info file
+                tarfile.open(tmp).extract(os.path.splitext(file)[0]
+                                          +"/job_info")
+                file_name = file_name+"/job_info"
+                
+                file_here = os.path.join(abs_dirs[i], file_name)
+                file_paths.append(file_here)
+
+    # List of directories with file_name (e.g. job_info)
     return file_paths
 
 
@@ -293,7 +340,7 @@ def get_frac(covered, no_cover):
 def output_coverage(output_name, covered, no_cover, covered_frac,
                     no_cover_frac, specific = []):
     # Prints the results of the coverage tests to coverage files
-    
+
     
     with open(output_name, mode='w') as coverage:
         # Makes a file and to list the parameters that were not covered
@@ -328,6 +375,17 @@ def remove_duplicates(all_params):
 
     return list(set(all_params)), len(list(set(all_params)))
 
+def clean(file_paths):
+    # Removes all of the files and directories created to produce the
+    # coverage reports, but it will not delete the coverage reports
+
+
+    for i in range(0,len(file_paths)):
+        
+        os.remove(file_paths[i])
+        directory = file_paths[i].replace("/job_info","")
+        os.rmdir(directory)
+
 
 if __name__ == '__main__':
-    main('maestro-overview.out')
+    main()
